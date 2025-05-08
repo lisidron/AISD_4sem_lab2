daad7156498adf7d0c008815392046f22f647da0
Elisabeth Kalinkina <153385301+lisidron@users.noreply.github.com>
2025-05-08 22:24:53 +0300

    Update brain.py

--- brain.py
+++ brain.py
@@ -3,7 +3,4 @@ from decompressor import decompress_image
 
 for i in range(20, 100, 20):
     compress_image("data/sea.png", "data/sea.raw", quality=i)
-    # display_compressed_image("data/Tree.raw")
     decompress_image("data/sea.raw", f"sea/sea {i}.png")
-
-# compress_image("data/Lenna.png", "data/Lenna.raw", quality=1)
